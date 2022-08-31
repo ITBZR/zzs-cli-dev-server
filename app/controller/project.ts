3bd5842a9ec427b3d758b18e652c20c8a720be26
xyj <296576733@qq.com>
2022-08-31 17:55:24 +0800

    fix: conf

--- app/controller/project.ts
+++ app/controller/project.ts
@@ -2,7 +2,7 @@
  * @Descripttion: 项目模板
  * @Author: BZR
  * @Date: 2022-08-26 10:16:44
- * @LastEditTime: 2022-08-29 15:31:23
+ * @LastEditTime: 2022-08-31 14:37:45
  */
 import { Controller } from 'egg';
 import { Project } from '../utils/shcema/project';
@@ -14,6 +14,9 @@ export default class ProjectController extends Controller {
     //   name: 'vue3 + ts + bemcss + vite',
     //   npmName: 'zzs-cli-dev-template-vue3',
     //   version: '1.0.0',
+    //   type: 'normal',
+    //   startCommand: 'npm run dev',
+    //   installCommand: 'npm i',
     // });
     // await project.save();
     const res = await Project.find();
--- app/utils/shcema/project.ts
+++ app/utils/shcema/project.ts
@@ -1,8 +1,17 @@
+/*
+ * @Descripttion: 集合
+ * @Author: BZR
+ * @Date: 2022-08-29 11:42:14
+ * @LastEditTime: 2022-08-31 14:24:50
+ */
 import mongoose from 'mongoose';
 
 const ProjectSchema = new mongoose.Schema({
   name: String,
   npmName: String,
   version: String,
+  type: String,
+  installCommand: String,
+  startCommand: String,
 });
 export const Project = mongoose.model('Project', ProjectSchema);
